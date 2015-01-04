c30d1931f655bf60b26ab848c3b8ce1104bda967
Pan Chuan <532601270@qq.com>
2015-01-04 21:55:11 +0900

    begin to code with problem 0001

---
+++ PanChuan/0001/generate_coupon.py
@@ -0,0 +1,12 @@
+
+
+"""
+generate total_num coupons,every coupon consists of
+coupon_digit which are capital letter or 0~9 
+"""
+generate_coupon(coupon_digit,total_num):
+	for i in range(total_num:
+		for j in range(coupon_digit):
+			
+		
+	

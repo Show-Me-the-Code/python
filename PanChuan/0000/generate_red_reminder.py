efb3cdfb1d97ac2f69d1636104fb601b5e53ec11
Pan Chuan <532601270@qq.com>
2015-01-05 19:37:42 +0900

    make 0000 done

--- PanChuan/0000/generate_red_reminder.py
+++ PanChuan/0000/generate_red_reminder.py
@@ -2,6 +2,9 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+"""
+make an image's white part to be transparent for merging with another image
+"""
 def white_to_transparent(img):
 	img = img.convert('RGBA')
 	datas = img.getdata()
@@ -14,16 +17,17 @@ def white_to_transparent(img):
 	img.putdata(newData)
 	return img
 
-p1_name = "github_logo.png"
-p2_name = "red_reminder.png"
-p1_image = Image.open(p1_name)
-p2_image = Image.open(p2_name)
-p2_transparent = white_to_transparent(p2_image)
-p1_image.paste(p2_transparent,(0,0),p2_transparent)
+if __name__ == "__main__":
+	p1_name = "github_logo.png"
+	p2_name = "red_reminder.png"
+	p1_image = Image.open(p1_name)
+	p2_image = Image.open(p2_name)
+	p2_transparent = white_to_transparent(p2_image)
+	p1_image.paste(p2_transparent,(0,0),p2_transparent)
 
-usr_font = ImageFont.truetype("./yahei.TTF",32)
-draw = ImageDraw.Draw(p1_image)
-draw.text((152,8),u'12',font = usr_font)
-p1_image.save("final.png","PNG")
+	usr_font = ImageFont.truetype("./yahei.TTF",32)
+	draw = ImageDraw.Draw(p1_image)
+	draw.text((152,8),u'12',font = usr_font)
+	p1_image.save("final.png","PNG")
 
 

d7a960b8083d71ddcbbb00d7a3fc390b10d7b0b2
razzl <razzl@users.noreply.github.com>
2015-03-30 08:08:42 +0800

    Update 0000.py

--- razzl/0000.py
+++ razzl/0000.py
@@ -12,7 +12,7 @@ def draw_number(path = '/root/Desktop/1.jpg',num = 4):
 	size = im.size#get the size of the picture
 	fontsize = size[0]/4
 	draw = ImageDraw.Draw(im)#the ImageDraw.Draw will rerturn a object then you can draw it
-	font = ImageFont.truetype('/usr/share/fonts/dejavu/DejVuSans.ttf',size[0]/4)#define the font and sieze of the number 
+	font = ImageFont.truetype('/usr/share/fonts/dejavu/DejVuSans.ttf',size[0]/4)#define the font and size of the number 
 	draw.text((3*fontsize,0),str(num),(255, 0, 0),font)#draw it 
 	im.save('/root/Desktop/2.jpg')#save
 draw_number()

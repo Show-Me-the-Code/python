f9d3a516db983cccd3d1d66130bca0c0fab8aca3
Matafight <guo_sc@foxmail.com>
2015-04-16 23:29:10 +0800

    add 0007

--- acwong00/0000/test.py
+++ acwong00/0000/test.py
@@ -6,6 +6,6 @@ fnt = ImageFont.truetype("msyh.ttf", 80)
 
 d = ImageDraw.Draw(original)
 
-d.text((200, 0), "7", font=fnt, fill=(255,0,0,255))
+d.text((200, 0), "6", font=fnt, fill=(255,0,0,255))
 
 original.save("finnal.jpg")

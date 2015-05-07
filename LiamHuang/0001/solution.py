43bb6f248188116aeb7a9d9eabec6a022365e3fc
Liam Huang <liamhuang0205@gmail.com>
2015-05-07 21:41:01 +0800

    LiamHuang: 0001 - 增加笔记内容

--- LiamHuang/0001/solution.py
+++ LiamHuang/0001/solution.py
@@ -3,6 +3,9 @@
 '''
 做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
 使用 Python 如何生成 200 个激活码（或者优惠券）？
+
+笔记参见：
+http://liam0205.me/2015/05/07/generator-of-invitation-code-in-python/
 '''
 
 import random, string

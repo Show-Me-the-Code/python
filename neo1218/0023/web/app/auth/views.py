922ff6d267d58c3fb2d337f1624895c97a3b6897
neo1218 <neo1218@yeah.net>
2015-05-11 23:18:02 +0800

    0023 yes

---
+++ neo1218/0023/web/app/auth/views.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/python
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user, logout_user, login_required, \
+    current_user
+from . import auth
+from .. import db
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('用户名或密码不正确!')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('你已经退出!.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('你现在可以登录了!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)

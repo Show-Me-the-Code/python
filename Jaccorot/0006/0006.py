218ea263f2d22b7f5ebd8b704e8c4bde5f865dec
Jaccorot <caozijun007@163.com>
2015-10-15 13:21:40 +0800

    add 0006

--- Jaccorot/0006/0006.py
+++ Jaccorot/0006/0006.py
@@ -7,10 +7,31 @@
 """
 
 import os
-
+import re
 
 def walk_dir(path):
-	for root, dirs, files in os.walk(path):
-		for f in files:
-			if f.lower().endswith('txt'):
-				
+    file_path = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.lower().endswith('txt'):
+                file_path.append(os.path.join(root, f))
+    return file_path
+
+def find_key_word(filepath):
+    word_dic = {}
+    filename = os.path.basename(filepath)
+    with open(filepath) as f:
+        text = f.read()
+        words_list = re.findall(r'[A-Za-z]+', text.lower())
+        for w in words_list:
+            if w in word_dic:
+                word_dic[w] += 1
+            else:
+                word_dic[w] = 1
+        sorted_word_list = sorted(word_dic.items(), key=lambda d: d[1])
+        print u"在%s文件中，%s为关键词，共出现了%s次" %(filename, sorted_word_list[-1][0], sorted_word_list[-1][1])
+
+if __name__ == "__main__":
+    for file_path in walk_dir(os.getcwd()):
+        find_key_word(file_path)
+

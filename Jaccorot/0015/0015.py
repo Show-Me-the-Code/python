9bff757d94873b438bc42294973d78a94d8df9a7
Jaccorot <caozijun007@163.com>
2015-10-26 21:14:44 +0800

    fix 0014,add 0015 0016

--- Jaccorot/0015/0015.py
+++ Jaccorot/0015/0015.py
@@ -2,8 +2,8 @@
 # coding=utf-8
 
 """
-第 0014 题： 纯文本文件 student.txt为学生信息, 里面的内容（包括花括号）如下所示,
-请将上述内容写到 student.xls 文件中，如下图所示：
+纯文本文件 city.txt为城市信息, 里面的内容（包括花括号）如下所示：
+请将上述内容写到 city.xls 文件中，如下图所示：
 """
 
 import os
@@ -19,15 +19,14 @@ def read_txt(path):
 
 def save_into_excel(content_dict, excel_name):
     wb = xlwt.Workbook()
-    ws = wb.add_sheet("student",  cell_overwrite_ok=True)
+    ws = wb.add_sheet("city",  cell_overwrite_ok=True)
     row = 0
     col = 0
 
     for k, v in sorted(content_dict.items(),key=lambda d:d[0]):
         ws.write(row, col, k)
-        for i in v:
-            col += 1
-            ws.write(row, col, i)
+        col += 1
+        ws.write(row, col, v)
 
         row += 1
         col = 0
@@ -36,7 +35,5 @@ def save_into_excel(content_dict, excel_name):
 
 
 if __name__ == "__main__":
-    read_content = read_txt(os.path.join(os.path.split(__file__)[0], 'student.txt'))
-    save_into_excel(read_content, 'student.xls')
-
-
+    read_content = read_txt(os.path.join(os.path.split(__file__)[0], 'city.txt'))
+    save_into_excel(read_content, 'city.xls')

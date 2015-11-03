40c5e11d292b8bb1e2cfb8f18ad650646bca779f
Vincent Zhang <vvzwvv@gmail.com>
2015-11-03 17:06:48 +0800

    Problem 0012 solved

---
+++ vvzwvv/0012/0012.py
@@ -0,0 +1,22 @@
+from cmd import Cmd
+
+class CmdCheck(Cmd):
+	
+	def __init__(self):
+		Cmd.__init__(self)
+		self.intro = "-------- Word Sensor (Input \"exit\" for termination) --------"
+		self.prompt = "> "
+		self.sensitive = map(lambda word: word.strip("\n"), open("filtered_words.txt").readlines())
+
+	def default(self, line):
+		for word in self.sensitive:
+			if word in line:
+				line = line.replace(word, ''.join("*" for i in range(len(word))))
+		print line
+
+	def do_exit(self, line):
+		exit()
+
+if __name__ == "__main__":
+	cmd = CmdCheck()
+	cmd.cmdloop()

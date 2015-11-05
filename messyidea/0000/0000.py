2a8702243165f79adc33d5ad398740f443cb5a96
messyidea <the.mid.sea@gmail.com>
2015-11-05 13:25:37 +0800

    fix some bugs on problem 0000

--- messyidea/0000/0000.py
+++ messyidea/0000/0000.py
@@ -3,8 +3,8 @@ import Image, ImageDraw, ImageFont
 
 org = Image.open('./origin.jpg')
 draw = ImageDraw.Draw(org)
-font = ImageFont.truetype('Arial.ttf', 36)
 w, h = org.size
 fontsize = min(w, h) / 4
-draw.text((org.size[0] - fontsize), '3', font = font, fill = (255, 0, 0))
+font = ImageFont.truetype('GenBkBasI.ttf', fontsize)
+draw.text((w - fontsize, 0), '3', font = font, fill = (255, 0, 0))
 org.save('rst.jpg', 'jpeg')

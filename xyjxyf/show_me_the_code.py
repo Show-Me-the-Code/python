5b0d05bed003185b335774797de4c63355dc6ecb
XieYajie <xyjxyf@163.com>
2016-03-18 10:41:16 +0800

    将字典类型的文本写入xls

--- xyjxyf/show_me_the_code.py
+++ xyjxyf/show_me_the_code.py
@@ -53,7 +53,6 @@ def save_activation_code_to_mysql():
 
 
 # 第 0003 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 Redis 非关系型数据库中。
-
 import redis
 
 def save_activation_code_to_redis():
@@ -253,6 +252,48 @@ def replace_sensitive_words(sensitive_file=None, input_string=None):
 
 # 第 0013 题： 用 Python 写一个爬图片的程序
 
+# 第 0014 题： 纯文本文件 student.txt为学生信息, 写到 student.xls 文件中
+import json
+import xlwt
+
+def dictxt_to_xls(file_path=None):
+    if file_path is None:
+        return
+
+    file = open(file_path, 'r')
+    if file is None:
+        return
+
+    content = json.loads(file.read())
+    list_data = sorted(content.items(), key=lambda d:d[0])
+
+    (path, name)=os.path.split(file.name)
+    file_name = name.split('.')[0]
+
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet(file_name)
+
+    row = 0
+    for item in list_data:
+        col = 0
+        ws.write(row, col, item[0])
+        col += 1
+
+        value = item[1]
+        if type(value) == list:
+            for obj in value:
+                ws.write(row, col, obj)
+                col += 1
+        else:
+             ws.write(row, col, value)
+        row += 1
+
+    save_path = path + "/" + file_name + ".xls"
+    wb.save(save_path)
+
+
+# 第 0015 题： 纯文本文件 city.txt为城市信息,写到 city.xls 文件中
+
 
 if __name__ == "__main__":
     # 0000
@@ -296,8 +337,10 @@ if __name__ == "__main__":
     # 0013
 
     # 0014
+    # dictxt_to_xls("./0014/student.txt")
 
     # 0015
+    # dictxt_to_xls("./0015/city.txt")
 
     # 0016
 

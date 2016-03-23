07fa80dd2b6f4df5501179b1531daf81340751c8
sandeepbvv11 <sandeepbvv11@gmail.com>
2016-03-23 10:47:33 +0530

    Updated 0001.py

--- sandeepbvv11/0001/0001.py
+++ sandeepbvv11/0001/0001.py
@@ -1,9 +1,14 @@
 import uuid
 import random
 f=open('IDs.txt','w')
+st= 'qwertyuiopasdfghjklzxcvbnm!@#$%^&*'
 for i in range(0,200):
-    x=str(uuid.uuid4().fields[-1])[:10]
-    f.write(x+"\n")
+    x=str(uuid.uuid4().fields[-1])[:10]+st
+    y=''
+    for j in range(0,10):
+         y+=random.choice(x)
+    print (y+"\n")
+    f.write(y+"\n")
 f.close()
 
 

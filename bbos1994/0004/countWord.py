26f7e784278cb25988c00efe078666fa86f61db8
bbos1994 <bbos1994@163.com>
2016-05-17 22:53:38 +0800

    modified algorithm

--- bbos1994/0004/countWord.py
+++ bbos1994/0004/countWord.py
@@ -3,6 +3,7 @@
 
 __author__='TonyZhu'
 
+import re
 # import os
 
 # print(os.getcwd()+'/words.txt')
@@ -10,19 +11,21 @@ def wordStatistics(path):
 	wordDict = {}
 	with open(path,'r') as file:
 		for line in file:
-			wordListPerLine = line.strip().split(' ')
-			for word in wordListPerLine:
-				word = word.lower()
-				if wordDict.has_key(word):
-					wordCount = wordDict.get(word)
-					wordCount+=1
-					wordDict[word] = wordCount
-				else:
-					wordDict[word] = 1
+			wordsSplitByPunc = re.findall(r'[a-z0-9]+',line.lower())
+			for words in wordsSplitByPunc:
+				wordList = words.strip().split() # split by space
+				for _word in wordList:
+					if wordDict.has_key(_word):
+						wordCount = wordDict.get(_word)
+						wordCount+=1
+						wordDict[_word] = wordCount
+					else:
+						wordDict[_word] = 1
+			# wordListPerLine = line.lower().replace(string.punctuation,'').strip().split(' ')
 	return wordDict
 
 if __name__ == '__main__':
 	import os
 	wordDict = wordStatistics(os.getcwd()+'/word.txt')
 	for k,v in wordDict.items():
-		print('key is %s,value is %s'%(k,v))
+		print('key is \'%s\',value is %s'%(k,v))

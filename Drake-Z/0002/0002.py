5be83a86a881895efadd61b786935d0a3b09e982
Drake-Z <drake.z.merlin@gmail.com>
2016-06-16 20:51:44 +0800

    Fix note

--- Drake-Z/0002/0002.py
+++ Drake-Z/0002/0002.py
@@ -14,7 +14,7 @@ def write_to_mysql(filename):
     cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
     f = open(filename, 'r').readlines()
     for line, num in zip(f, range(1, len(f)+1)):
-        line = line[:-1]                     #直接除去最后一个字符的所有字符，这里去除\n符号
+        line = line[:-1]                     #去除\n符号
         cursor.execute('insert into user (id, name) values (%s, %s)', [str(num), line])
         conn.commit()
     cursor.close()

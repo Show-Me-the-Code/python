0043b02553e20a176a09c686f5694cfac81c7829
Drake-Z <drake.z.merlin@gmail.com>
2016-06-17 10:38:15 +0800

    Add 0007 file

---
+++ Drake-Z/0007/test.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# mydict_test.py
+
+import unittest
+from MyMoudel.Moudel1 import Dict
+class TestDict(unittest.TestCase):
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
+if __name__ == '__main__':
+    unittest.main()

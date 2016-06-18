a3ee5a2be169cd867f4e16e74adff9ff8772a985
Drake-Z <drake.z.merlin@gmail.com>
2016-06-18 09:44:13 +0800

    Fix 0016

--- Drake-Z/0016/0016.py
+++ Drake-Z/0016/0016.py
@@ -11,38 +11,20 @@
 
 __author__ = 'Drake-Z'
 
-import os
-import re
-from collections import OrderedDict
-import xlwt
+import json
+from openpyxl import Workbook
+from openpyxl.styles import Font
 
-def shuju(data, re1, re2):
-    c = OrderedDict([])
-    re_xuhao = re.compile(r'%s' % re1)            
-    re_yuansu = re.compile(r'%s' % re2)            
-    a = re_xuhao.findall(data)                      #得到序号
-    if len(a)==0:
-        for d in range(0, hangshu):
-            a.append(d)
-    b = re_yuansu.findall(data)                 #得到具体数据
-    for m, n in zip(a, b):                          #将数据转为Dict
-        n = re.split(r',', n)
-        c[m] = n
-    writeFlie(c, hangshu, lieshu)
-
-def writeFlie(dictdata, hangshu, lieshu):
-    workbook = xlwt.Workbook(encoding = 'utf-8')                #创建工作薄
-    worksheet = workbook.add_sheet('My Worksheet')              #创建表
-    num = list(dictdata.keys())                                                    #得到序号
-    for i in range(0, hangshu):
-        for m in range(0, lieshu):
-            worksheet.write(i, m, label = dictdata[num[i]][m])
-    workbook.save('numbers.xls')
+def txt_to_xlsx(filename):
+    file = open(filename, 'r', encoding = 'UTF-8')
+    file_cintent = json.load(file, encoding = 'UTF-8')
+    print(file_cintent)
+    workbook = Workbook()
+    worksheet = workbook.create_sheet(title = 'numbers')
+    for i in range(1, len(file_cintent)+1):
+        for m in range(1, len(file_cintent[i-1])+1):
+            worksheet.cell(row = i, column = m).value = file_cintent[i-1][m-1]
+    workbook.save(filename = 'numbers.xls')
 
 if __name__ == '__main__':
-    file = open('numbers.txt', 'r', encoding='utf-8')
-    hangshu = 3
-    lieshu = 3
-    re1 = '%'
-    re2 = '\[(.*?)\]'
-    shuju(file.read(), re1, re2)
+    txt_to_xlsx('numbers.txt')

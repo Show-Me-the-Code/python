645b67f138f98c70957ef6541a1a23560d4da2c0
houshengandt <houshengandt@gmail.com>
2016-08-01 13:53:06 +0800

    0000

--- houshengandt/0000/0000.py
+++ houshengandt/0000/0000.py
@@ -1,32 +1,18 @@
-import os
 from PIL import Image, ImageDraw, ImageFont
-from random import randint
 
 
-# s = "3"
-# a = Image.open("1.jpg")
-# f = ImageFont.truetype("arial.ttf", 40)
-# b = ImageDraw.Draw(a)
-# b.text((90, 90), s, fill='red', font=f)
-# a.save("2.jpg")
-
-
-
-
-def add_num(filename='1.jpg', text='?', size=40, color='red', newname="2.jpg"):
+def add_num(filename, text='9', size=40, color='red'):
     a = Image.open(filename)
     print(a.size)
     f = ImageFont.truetype("arial.ttf", size)
     b = ImageDraw.Draw(a)
     x, y = a.size
-    xy = (x - 40, y - 170)
+    xy = (x - 40, y - 190)
     b.text(xy, text, fill=color, font=f)
+    newname = "new" + filename
     a.save(newname)
 
+if __name__ == "__main__":
+    test = add_num("1.jpg")
 
-# add_num(text="3")
 
-for root, dirs, files in os.walk("C:\\Users\\housh\\Desktop\\tx"):
-    for file in files:
-        print(type(file))
-        add_num("C:\\Users\\housh\\Desktop\\tx\\" + file, newname='new' + file)
---
+++ houshengandt/0001/0001.py
@@ -0,0 +1,6 @@
+import uuid
+
+
+for i in range(200):
+    keys = str(uuid.uuid4())
+    print(keys)

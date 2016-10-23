b6b6372ba673982f4278f0de26dd252e06b83940
BrambleXu <zhuixiyou@gmail.com>
2016-10-23 21:17:40 +0900

    add some comment about the position

--- BrambleXu/play0/play0.py
+++ BrambleXu/play0/play0.py
@@ -11,14 +11,21 @@ fill – Color to use for the text.
 font – An ImageFont instance.
 '''
 
+'''
+1. 坐标 (0, 0) 表示左上角，整个图片类似镶嵌在坐标的第四像限；img.size 是一个二位数组，依次位图片的宽和高。
+2. 在图片上添加内容时注意对应内容也会占位置，所以不能当作点即 pixel 来看待，实际处理时需要考虑添加对象的 size，这就对应代码中右上角坐标position的获取。
+3. ImageFont 的获取有很多方法，其中较简单的方法从 truetype 中获取，对应 font 存放的位置一般是 /usr/share/fonts/...
+'''
+
 from PIL import Image, ImageDraw, ImageFont
 
 def add_num(picPath, num):
     img = Image.open(picPath)
     xSize, ySize = img.size
-    #fontsize = min(xSize, ySize)/4
-    myfont = ImageFont.truetype('Futura.ttf', xSize/4)
-    ImageDraw.Draw(img).text((ySize//5 * 4, 0), str(num), font = myfont, fill='red')
+    fontsize = ySize / 4
+    position = xSize - fontsize
+    myfont = ImageFont.truetype('Futura.ttf', fontsize)
+    ImageDraw.Draw(img).text((position, 0), str(num), font = myfont, fill='red')
     img.save('icon_with_num.jpg')
 
 if __name__ == '__main__':

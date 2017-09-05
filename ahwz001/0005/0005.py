562bdf1305d8389c509d3602f23184c93d49f388
tux <ahwz001@qq.com>
2017-09-05 22:21:03 +0800

    finished 0005

---
+++ ahwz001/0005/0005.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+* 0005
+* by ahwz001
+* 2017/8/10
+* Thanks for this project. I learned a lot here.
+"""
+
+import os
+import re
+from PIL import Image
+
+Start_path = './img_test/'
+Out_path = './img_out/'
+
+
+def deal_img(bill):
+    count = 0
+    for pic in bill:
+        path = Start_path + pic
+        print path
+        im = Image.open(path)
+        out = im.resize((1136,640))
+        # out = im.resize((1136,640),Image.ANTIALIAS)
+        new_pic = re.sub(pic[:-4],pic[:-4]+'_new',pic)
+        print new_pic
+        new_path = Out_path + new_pic
+        out.save(new_path)
+        count += 1
+        print "the img " + pic + " has been modifed."
+
+    print "%d imgs has been modified." % count
+
+
+def thumbnail_pic(bill):
+    for x in bill:
+        print x
+        name = os.path.join(Start_path, x)
+        im = Image.open(name)
+        im.thumbnail((1136, 640))
+        print(im.format, im.size, im.mode)
+        new_pic = Out_path + x
+        im.save(new_pic)
+    print('END')
+
+
+def main():
+    bill = os.listdir(Start_path)
+    print bill
+    thumbnail_pic(bill)
+    # deal_img(bill)
+
+
+if __name__ == '__main__':
+    main()

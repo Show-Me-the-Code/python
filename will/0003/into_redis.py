f6e93144a2471ef22883f4db935a499463a76824
Will <i@will66.me>
2017-09-17 00:05:38 +0800

    fix sytanx errors

--- will/0003/into_redis.py
+++ will/0003/into_redis.py
@@ -15,7 +15,7 @@ def gen2():
 def gen3():
     return uuid.uuid4()
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     r = redis.Redis(host='localhost', port=6379, db=0)
     # r.set('name', 'will')
     # print(r.get('name'))

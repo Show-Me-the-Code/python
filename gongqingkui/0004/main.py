b93c2d0abb68341b2d2d69a9e853472cffc568ff
gongqingkui <gongqingkui@126.com>
2018-01-22 10:47:56 +0800

    0004 completed

---
+++ gongqingkui/0004/main.py
@@ -0,0 +1,25 @@
+#0004 riji
+filename='01.txt'
+wordarrays=[]
+worddict={}
+xucis=['a','an','The','by','the','in','on','from','above','behind','is','and','but','before','oh','well','hi','of']
+with open(filename) as f:
+    l = f.readline()
+    while l:
+        wordarrays.append(l.split())
+        l = f.readline()
+
+for wordarray in wordarrays:
+    for word in wordarray:
+        word = word.lower()
+        if word in worddict:
+            worddict[word] = worddict.get(word) + 1
+        elif word not in worddict:
+            worddict[word] = 1
+            
+
+for xuci in xucis:
+    if xuci in worddict:
+        del worddict[xuci]
+sortedwordlist = sorted(worddict.items(),lambda x,y:cmp(x[1],y[1]),reverse=True)
+print(sortedwordlist)

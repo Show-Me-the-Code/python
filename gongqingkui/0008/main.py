13a5d7f9233a0b790ae52d32f968ee2917a7a7b3
gongqingkui <gongqingkui@126.com>
2018-01-24 23:18:54 +0800

    0008 0009 0010 completed

---
+++ gongqingkui/0008/main.py
@@ -0,0 +1,16 @@
+#0008
+import urllib2
+from bs4 import BeautifulSoup
+import re
+
+url="http://www.iscas.ac.cn/xwdt2016/rdxw2016/201712/t20171201_4905565.html"
+
+def processHTML(html):
+    soup = BeautifulSoup(html)
+    for d in soup.find_all(id="xlmain"):
+        if d.name in ['p','div']:
+            print(d.text)
+    
+if __name__ =="__main__":
+    html = urllib2.urlopen(url).read()
+    processHTML(html)

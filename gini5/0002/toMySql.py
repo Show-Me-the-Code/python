f2e08be5fe59d881fbaae9180a7d1050bb5ff3cb
Gini Hu <gini.hu@sap.com>
2018-06-26 18:00:43 +0800

    0002
    connect to db and finish 0002

--- gini5/0002/toMySql.py
+++ gini5/0002/toMySql.py
@@ -1,24 +1,31 @@
-import pymysql, random, string
-db = pymysql.connect(host="localhost",user="testuser",passwd="test123",db="mysql")
+import MySQLdb, random, string
+db = MySQLdb.connect(host="localhost", port = 3306, user="root",passwd="test123",db="mysql")
 cursor = db.cursor()
-# dic = {}
-#
-# def generate_string(size=8):
-#     seed = string.ascii_letters+string.digits
-#     res = ''.join(random.sample(seed,size))
-#     return res
-#
-# for i in range(200):
-#     new = generate_string()
-#     while new in dic.values():
-#         new = generate_string()
-#     dic[i] = new
-#     cursor.execute("INSERT INTO ACTIVATIONCODE VALUES(%d,%s)"%(i,new))
-#
-# try:
-#     db.commit()
-# except:
-#         db.rollback()
 
+
+dic = {}
+
+def generate_string(size=8):
+    seed = string.ascii_letters+string.digits
+    res = ''.join(random.sample(seed,size))
+    return res
+
+for i in range(200):
+    new = generate_string()
+    while new in dic.values():
+        new = generate_string()
+    dic[i] = new
+    query = "INSERT INTO ACTIVATION_CODE VALUES(%d,%r)"%(i,new)
+    cursor.execute(query)
+
+try:
+    db.commit()
+except:
+        db.rollback()
+
+# select #
+cursor.execute('SELECT * FROM ACTIVATION_CODE')
+data = cursor.fetchall()
+print(data)
 cursor.close()
 db.close()

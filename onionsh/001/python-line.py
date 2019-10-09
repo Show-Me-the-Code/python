192d35345219d4f8680c3fd9f7cc21c4a8a64220
onion.sh <onionnake@gmail.com>
2019-10-09 13:11:12 +0700

    python send message to line

---
+++ onionsh/001/python-line.py
@@ -0,0 +1,19 @@
+from songline import Sendline
+
+token = 'token here' 
+messenger = Sendline(token)
+
+messenger.sendtext('Hello world')
+
+#send message 
+# messenger.sendtext('Hello world')
+
+#send sticker 
+# messenger.sticker(1,1)
+
+#send image 
+# messenger.sendimage('https://img.pngio.com/python-logo-python-logo-png-268_300.png')
+
+#Cr. 
+# https://pypi.org/project/songline/
+# https://line.me

a6b839f76c72c8f5ed4a91247dbe661c8738892f
Galuh Muhammad Iman Akbar <36703516+galuh404@users.noreply.github.com>
2020-10-02 19:58:55 +0800

    Update web_scraper.py

--- sec/web_scraper.py
+++ sec/web_scraper.py
@@ -1,6 +1,6 @@
 import requests
 
-proxies = {"http":"115.178.228.219:8080"}
+proxies = {"http":"<IP>:8080"}
 headers  = {"user-agent":"Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0"}
 
 r = requests.get("http://testing-ground.scraping.pro/whoami", proxies=proxies, headers=headers)
